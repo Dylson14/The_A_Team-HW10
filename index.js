de630583c2f131211d98cafe1c72e3ef59095ab2
dylson <dylsonuni@gmail.com>
2021-08-19 12:48:44 +0100

    passed in inquirer 4 the 1st time, created qst var

---
+++ index.js
@@ -0,0 +1,21 @@
+var inquirer = require('inquirer');
+
+const questions = [];
+
+
+
+
+
+
+
+inquirer.prompt(questions)
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
